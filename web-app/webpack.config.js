bbb9a2cb6e50c3075471f0438fe56a204aca7600
RJPearson94 <robpearson@pearson.co.uk>
2019-06-10 22:24:57 +0100

    add webpack to build code and docker to run the app

---
+++ web-app/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/server.ts',
+	devtool: 'source-map',
+	target: 'node',
+	
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	plugins: [
+		new webpack.ProgressPlugin()
+	],
+
+	module: {
+		rules: [
+			{
+				test: /.(ts|tsx)?$/,
+				loader: 'awesome-typescript-loader',
+				exclude: /node_modules\/(?!(inversify-shared)\/).*/,
+			}
+		]
+	},
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					priority: -10,
+					test: /[\\/]node_modules[\\/]/
+				}
+			},
+
+			chunks: 'async',
+			minChunks: 1,
+			minSize: 30000,
+			name: true
+		}
+	},
+
+	resolve: {
+		plugins: [
+			new TsConfigPathsPlugin({
+				'configFileName': 'tsconfig.json',
+				'useBabel': true,
+				'babelCore': '@babel/core'
+			})
+		],
+		extensions: [
+			'.tsx', 
+			'.ts', 
+			'.js'
+		]
+	}
+};
