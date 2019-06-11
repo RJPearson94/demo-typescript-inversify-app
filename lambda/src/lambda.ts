4b9f77ece681cb3069fad0ddd088cdabf1ef0da5
RJPearson94 <robpearson@pearson.co.uk>
2019-06-12 00:04:15 +0100

    Add Lambda Deployment

---
+++ lambda/src/lambda.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+import container from '@src/inversify.config';
+import TYPES from '@src/constant/types';
+import GreetingController from '@src/controller/greeting';
+
+const greetingController: GreetingController = container.get<GreetingController>(TYPES.GreetingController);
+
+exports.handler = function(event: any, context: any, callback: Function) {
+    const helloResponse = greetingController.greet()
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: helloResponse,
+        }),
+    });
+}
