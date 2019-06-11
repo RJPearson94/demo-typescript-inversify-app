aec4c6ed49daea1d85cc769efcd2c6edcd6e18a5
RJPearson94 <robpearson@pearson.co.uk>
2019-06-12 00:48:23 +0100

    Add unit tests for lambda function

---
+++ lambda/test/lambda.spec.ts
@@ -0,0 +1,46 @@
+import 'reflect-metadata';
+import TYPES from "@src/constant/types";
+import GreetingController from "@src/controller/greeting";
+import container from "@src/inversify.config";
+
+describe("#Lambda", () => {
+
+    beforeEach(() => {
+        container.snapshot()
+    });
+
+    afterEach(() => {
+        container.restore()
+    });
+
+    test("Should return HelloTest when lambda is invoked", (done) => {
+        // Setup
+        const mockGreetFunction = jest.fn().mockReturnValue("HelloTest");
+        const greetingController: GreetingController = {
+            greet: mockGreetFunction
+        };
+
+        container.unbind(TYPES.GreetingController);
+        container.bind<GreetingController>(TYPES.GreetingController).toConstantValue(greetingController);
+
+        // Given
+        const event: any = {};
+        const context: any = {}
+
+        // When
+        const lambda = require("@src/lambda");
+        lambda.handler(event, context, (error: any, response: any) => {
+            expect(error).toBeNull();
+            expect(response).toBeDefined();
+
+            expect(response.statusCode).toEqual(200);
+            expect(JSON.parse(response.body)).toEqual({
+                message: "HelloTest"
+            });
+            done();
+        });
+
+        // Then
+        // The callback should assert
+    });
+});
