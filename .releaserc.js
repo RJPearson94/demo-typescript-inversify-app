727539eeb3ca1bd11e9bc4c60634c3c827f7710e
RJPearson94 <robpearson@pearson.co.uk>
2019-06-23 18:43:32 +0100

    chore: format releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -2,9 +2,5 @@ module.exports = {
   repositoryUrl: 'https://github.com/RJPearson94/demo-typescript-express-app.git',
   tagFormat: 'v${version}',
   branch: 'master',
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github'
-  ]
+  plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
 };
