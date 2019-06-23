25bc8822a70c8f01f96c6101250c5eccaee5012b
RJPearson94 <robpearson@pearson.co.uk>
2019-06-23 19:45:49 +0100

    docs: add commit linting

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   hooks: {
     'post-commit': 'yarn format:all & yarn lint:all',
-    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook'
+    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
   }
 };
