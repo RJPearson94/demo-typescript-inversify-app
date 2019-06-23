933e10c6c67637acb159abf903e6cfa803d2b395
RJPearson94 <robpearson@pearson.co.uk>
2019-06-23 19:28:10 +0100

    style: add commit msg hook

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   hooks: {
-    'pre-commit': 'yarn format:all & yarn lint:all'
+    'pre-commit': 'yarn format:all & yarn lint:all',
+    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook'
   }
 };
