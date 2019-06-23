23b0d738d66c765188e77334c36942ec905b1b17
RJPearson94 <robpearson@pearson.co.uk>
2019-06-23 19:10:22 +0100

    chore: increase formatting on husky hook

--- lambda/terratest/helper/modules/s3/main.tf
+++ lambda/terratest/helper/modules/s3/main.tf
@@ -1,6 +1,6 @@
 module "artefact_bucket" {
-  source        = "devops-workflow/s3-buckets/aws"
-  version       = "0.3.0"
-  names         = ["${var.name}"]
-  environment   = "dev"
+  source      = "devops-workflow/s3-buckets/aws"
+  version     = "0.3.0"
+  names       = ["${var.name}"]
+  environment = "dev"
 }
