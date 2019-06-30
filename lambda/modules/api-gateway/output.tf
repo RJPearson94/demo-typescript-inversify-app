a53d9d3188764a5fdba68087af388e11122c1333
RJPearson94 <robpearson@pearson.co.uk>
2019-06-30 13:49:52 +0100

    chore(terraform): format terraform

--- lambda/modules/api-gateway/output.tf
+++ lambda/modules/api-gateway/output.tf
@@ -1,4 +1,4 @@
 output "invoke_url" {
   description = "The URL to invoke an API stage"
-  value = aws_api_gateway_stage.greeting_stage.invoke_url
+  value       = aws_api_gateway_stage.greeting_stage.invoke_url
 }
