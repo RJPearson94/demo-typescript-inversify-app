f8fd7c875c4b7c65af3b7fbc2f40502f34f6a750
RJPearson94 <RJPearson94@users.noreply.github.com>
2019-07-02 23:35:27 +0100

    Address Tech Debt (#55)
    * Centralise tsconfig
    * Bump dependencies
    * Add linting TS files
    * Prettify Code
    
    fix #50, fix #51, fix #52, fix #53, fix #54

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{ts,js,json,yml,md}': ['yarn prettier', 'git add'],
+  '*.{ts,js,json,yml,md}': ['yarn prettier --write', 'git add'],
   '*.go': ['gofmt', 'git add'],
   '*.{tf,hcl}': ['terraform fmt', 'git add'],
   '*.{ts,js}': ['yarn lint --fix', 'git add']
