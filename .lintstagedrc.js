ca20033dc2352fea90146bc2e58954eb31688641
RJPearson94 <robpearson@pearson.co.uk>
2019-07-02 23:24:52 +0100

    chore(linting): fix lint-staged linting

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ module.exports = {
   '*.{ts,js,json,yml,md}': ['yarn prettier', 'git add'],
   '*.go': ['gofmt', 'git add'],
   '*.{tf,hcl}': ['terraform fmt', 'git add'],
-  '*.{ts,js}': ['yarn lint:all --fix', 'git add']
+  '*.{ts,js}': ['yarn lint --fix', 'git add']
 };
