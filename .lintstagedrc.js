4e0e6f1410830fd733b5bf906b2cb32c118ec695
RJPearson94 <robpearson@pearson.co.uk>
2019-07-10 20:04:09 +0100

    chore(lint-staged): update lint staged to not format hcl

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '*.{ts,js,json,yml,md}': ['yarn prettier --write', 'git add'],
   '*.go': ['gofmt', 'git add'],
-  '*.{tf,hcl}': ['terraform fmt', 'git add'],
+  '*.{tf}': ['terraform fmt', 'git add'],
   '*.{ts,js}': ['yarn lint --fix', 'git add']
 };
