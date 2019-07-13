4fd459ca81f163561025704339169da86bfb7be5
RJPearson94 <robpearson@pearson.co.uk>
2019-07-13 12:56:45 +0100

    chore: remove changelog
    changelog is not necessary as github releases are equilivant

--- .releaserc.js
+++ .releaserc.js
@@ -2,16 +2,5 @@ module.exports = {
   repositoryUrl: 'https://github.com/RJPearson94/demo-typescript-inversify-app.git',
   tagFormat: 'v${version}',
   branch: 'master',
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogTitle: 'Changelog for Demo Typescript Inversify App',
-        changelogFile: 'CHANGELOG.md'
-      }
-    ],
-    '@semantic-release/github'
-  ]
+  plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
 };
