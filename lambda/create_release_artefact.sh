5a371074aa22dbeb01819cdbb2011756439fac46
RJPearson94 <robpearson@pearson.co.uk>
2019-07-15 00:04:31 +0100

    chore: create release artefact
    create artefact which contains Terraform and lambda zips

---
+++ lambda/create_release_artefact.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+release_folder=release
+
+rm -rf $release_folder
+mkdir $release_folder
+
+find . -type d -name ".terragrunt-cache" -prune -o -type f | egrep "\.(zip|tf)$" | zip -@ $release_folder/inversify-lambda-demo-v$1.zip
