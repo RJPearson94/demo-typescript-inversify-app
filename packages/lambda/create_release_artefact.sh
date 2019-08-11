2b5a97c67528529d75aa27aec54dbaec0782558c
RJPearson94 <robpearson@pearson.co.uk>
2019-08-11 14:16:59 +0100

    fix: correct lambda artefact zip inclusion

--- packages/lambda/create_release_artefact.sh
+++ packages/lambda/create_release_artefact.sh
@@ -5,4 +5,4 @@ release_folder=release
 rm -rf $release_folder
 mkdir $release_folder
 
-find . -type d -name ".terragrunt-cache" -prune -o -type f | egrep "\.(zip|tf|tpl)$" | zip -@ $release_folder/inversify-lambda-demo-v$1.zip
+find . -type d -name ".terragrunt-cache" -prune -o -type f | egrep "\.(zip|tf|tpl.json)$" | zip -@ $release_folder/inversify-lambda-demo-v$1.zip
