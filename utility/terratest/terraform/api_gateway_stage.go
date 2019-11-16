9dc0f0e101608b473bf2846614b3f5551ae5a842
RJPearson94 <robpearson@pearson.co.uk>
2019-11-16 17:21:02 +0000

    chore: update api gateway stage terratest struct

--- utility/terratest/terraform/api_gateway_stage.go
+++ utility/terratest/terraform/api_gateway_stage.go
@@ -4,6 +4,7 @@ import "github.com/zclconf/go-cty/cty"
 
 type APIGatewayStage struct {
 	AccessLogSettings    []string          `cty:"access_log_settings"`
+	ARN                  cty.Value         `cty:"arn"`
 	CacheClusterEnabled  cty.Value         `cty:"cache_cluster_enabled"`
 	CacheClusterSize     cty.Value         `cty:"cache_cluster_size"`
 	ClientCertificateID  cty.Value         `cty:"client_certificate_id"`
